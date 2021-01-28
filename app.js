6a1a8f4cbe6e233f35cdd41b7fd87066126af595
Roman <romhanbizu@gmail.com>
2021-01-28 12:53:53 -0800

    new file

---
+++ app.js
@@ -0,0 +1,13 @@
+let userName = prompt('what is your name?');
+alert('hello ' + (userName) + ' welcome to my page');
+function food(){ 
+let quetionOne = prompt('Do you like salmon');
+if (quetionOne === 'yes'){
+  alert('Good choice salmon is delicious');
+} else{
+  alert('You should try it!');
+}
+
+}
+food();
+
